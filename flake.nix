2d6c9bc2e1e06e62959b7402183bef8fe82bdc9f
Nirmal Khedkar <nirmalhk7@gmail.com>
2025-02-13 23:21:41 -0700

    Configured k3d
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,44 +1,38 @@
 {
   description = "We go big or go home. Operations Center Mainflake";
+
+  # Define the inputs for the flake
   inputs = {
+    # Use the unstable branch of nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    # Include the k3d flake from a local path
     k3d = {
       url = "./flakes/k3d";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
+  # Define the outputs of the flake
   outputs = { self, nixpkgs, k3d }: {
-    packages = let
-      createPackage = system: let
-        pkgs = import nixpkgs { inherit system; };
-      in pkgs.stdenv.mkDerivation {
-        name = "operationscenter-milano";
-        src = ./.;
-        buildInputs = [ pkgs.hello ];
-        installPhase = ''
-          mkdir -p $out/bin
-          echo '#!/bin/sh' > $out/bin/operationscenter-milano
-          echo 'echo Hello, World!' >> $out/bin/operationscenter-milano
-          chmod +x $out/bin/operationscenter-milano
-        '';
-      };
-    in {
-      x86_64-linux = createPackage "x86_64-linux";
-      aarch64-darwin = createPackage "aarch64-darwin";
+    # Include k3d in the packages section
+    packages = {
+      x86_64-linux = k3d.defaultPackage.x86_64-linux;
+      aarch64-darwin = k3d.defaultPackage.aarch64-darwin;
     };
 
+    # Define the default package for each system
     defaultPackage = {
       x86_64-linux = self.packages.x86_64-linux;
       aarch64-darwin = self.packages.aarch64-darwin;
     };
 
+    # Define development shells
     devShells = let
       pkgs = import nixpkgs { inherit (self) system; };
     in {
       default = pkgs.mkShell {
         buildInputs = [
-          self.packages.${builtins.currentSystem}
           k3d.defaultPackage.${builtins.currentSystem}
         ];
       };
