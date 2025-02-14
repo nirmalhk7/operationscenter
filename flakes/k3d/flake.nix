c185b056462e5eef49713d7b1f89ac3482feb7b6
Nirmal Khedkar <nirmalhk7@gmail.com>
2025-02-13 23:34:00 -0700

    k3d fix
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- flakes/k3d/flake.nix
+++ flakes/k3d/flake.nix
@@ -23,9 +23,12 @@
 
       shellHook = ''
         # Create clusters
-        k3d cluster create milano-dev --tls-san "trusted.nirmalhk7.com" --tls-san "milano"
-        k3d cluster create milano-managed --servers 2 --agents 2 --tls-san "trusted.nirmalhk7.com" --tls-san "milano"
-        k3d cluster create milano-live --tls-san "trusted.nirmalhk7.com" --tls-san "milano"
+        echo "dev cluster: playground"
+        k3d cluster create milano-dev --k3s-arg "--tls-san 'trusted.nirmalhk7.com' --tls-san 'milano'"
+        echo "managed cluster: critical applications"
+        k3d cluster create milano-managed --servers 2 --agents 2  --k3s-arg "--tls-san 'trusted.nirmalhk7.com' --tls-san 'milano'"
+        echo "live cluster: internet exposed"
+        k3d cluster create milano-live  --k3s-arg "--tls-san 'trusted.nirmalhk7.com' --tls-san 'milano'"
 
         # Print client-certificate-data, client-key-data, and certificate-authority-data
         for cluster in milano-dev milano-managed milano-live; do
