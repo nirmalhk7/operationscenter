fb9d95c40814d573081a7199c3694dd24cf27c40
Nirmal Khedkar <nirmalhk7@gmail.com>
2026-05-15 12:14:17 -0600

    feat(proxmox): define firewall ipsets and security groups
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

---
+++ infrastructure/terra/proxmox/sg-managed.tf
@@ -0,0 +1,35 @@
+resource "proxmox_virtual_environment_cluster_firewall_security_group" "sg-managed" {
+  name    = "sg-managed"
+  comment = "Managed"
+
+  rule {
+    enabled = true
+    action  = "ACCEPT"
+    type    = "in"
+    comment = "Allow all inward access"
+    dest    = "+dc/ipset-mgd"
+    log     = "debug"
+  }
+
+  rule {
+    enabled = true
+    action  = "DROP"
+    type    = "in"
+    comment = "Drop packets from dev"
+    source  = "+dc/ipset-dev"
+    dest    = "+dc/ipset-mgd"
+    log     = "debug"
+  }
+
+
+  rule {
+    enabled = true
+    action  = "ACCEPT"
+    type    = "out"
+    source  = "+dc/ipset-mgd"
+    comment = "Allow outbound internet traffic"
+    log     = "debug"
+  }
+
+
+}
