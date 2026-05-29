d4ff26aaae377df49c830ee6d5492c6009f23826
Nirmal Khedkar <nirmalhk7@gmail.com>
2026-05-28 21:44:37 -0600

    feat(proxmox): Add Nginx outbound firewall rules
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- infrastructure/terra/proxmox/sg-managed.tf
+++ infrastructure/terra/proxmox/sg-managed.tf
@@ -23,6 +23,42 @@ resource "proxmox_virtual_environment_cluster_firewall_security_group" "sg-manag
 
   rule {
     enabled = true
+    action  = "ACCEPT"
+    type    = "out"
+    comment = "Allow Nginx to reach k8mgd for trusted subdomains"
+    source  = "172.16.0.101"
+    dest    = "172.16.0.105"
+    proto   = "tcp"
+    dport   = "443"
+    log     = "info"
+  }
+
+  rule {
+    enabled = true
+    action  = "ACCEPT"
+    type    = "out"
+    comment = "Allow Nginx to reach Proxmox dashboard"
+    source  = "172.16.0.101"
+    dest    = "172.16.0.1"
+    proto   = "tcp"
+    dport   = "8006"
+    log     = "info"
+  }
+
+  rule {
+    enabled = true
+    action  = "ACCEPT"
+    type    = "out"
+    comment = "Allow Nginx to reach OpenClaw robot service"
+    source  = "172.16.0.101"
+    dest    = "172.16.0.104"
+    proto   = "tcp"
+    dport   = "18789"
+    log     = "info"
+  }
+
+  rule {
+    enabled = true
     action  = "DROP"
     type    = "out"
     comment = "Drop outbound SSH"
