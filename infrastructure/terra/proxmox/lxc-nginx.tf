17d336cc3f1d3c3e2825495200cecac1fa0d6005
Nirmal Khedkar <nirmalhk7@gmail.com>
2026-06-01 13:11:11 -0600

    chore(infra/proxmox): disable firewall for nginx lxc container
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- infrastructure/terra/proxmox/lxc-nginx.tf
+++ infrastructure/terra/proxmox/lxc-nginx.tf
@@ -26,7 +26,7 @@ resource "proxmox_virtual_environment_container" "lxc-nginx" {
     bridge   = "wmnet"
     name     = "net0"
     enabled  = true
-    firewall = true
+    firewall = false
   }
 
   memory {
