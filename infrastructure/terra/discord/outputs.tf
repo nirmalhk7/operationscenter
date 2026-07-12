2392a053a6ea6dc93278e57f59658362dd3b2229
Nirmal Khedkar <nirmalhk7@gmail.com>
2026-07-12 17:37:06 -0600

    feat(flux): route reconciliation errors to Discord

--- infrastructure/terra/discord/outputs.tf
+++ infrastructure/terra/discord/outputs.tf
@@ -3,3 +3,9 @@ output "manager_chat_webhook_url" {
   value       = format("https://discord.com/api/webhooks/%s/%s", discord_webhook.manager_chat.id, discord_webhook.manager_chat.token)
   sensitive   = true
 }
+
+output "flux_reconciliation_alerts_webhook_url" {
+  description = "Discord webhook URL for Flux reconciliation alerts."
+  value       = format("https://discord.com/api/webhooks/%s/%s", discord_webhook.flux_reconciliation_alerts.id, discord_webhook.flux_reconciliation_alerts.token)
+  sensitive   = true
+}
--- infrastructure/terra/discord/webhook.tf
+++ infrastructure/terra/discord/webhook.tf
@@ -2,3 +2,8 @@ resource "discord_webhook" "manager_chat" {
   channel_id = discord_text_channel.manager_chat.id
   name       = "ops-center-alerts"
 }
+
+resource "discord_webhook" "flux_reconciliation_alerts" {
+  channel_id = "1504282225770889226"
+  name       = "flux-reconciliation-alerts"
+}
